f8ebac610807632ce5f109d96875e725e5b8c211
Roshan V Jacob <roshan.jacob@asl.mum.sst>
2024-11-01 00:51:36 +0530

    Intial Commit

---
+++ src/test/java/com/example/RealLanguage/RealLanguageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.RealLanguage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealLanguageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
