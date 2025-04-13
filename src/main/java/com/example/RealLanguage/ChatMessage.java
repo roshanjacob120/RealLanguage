00b1f213817b0de0bd701a8ffca12d15b06881c4
roshan.jacob <roshan.jacob@ACER1205.asl.mum.sst>
2025-04-13 14:41:48 +0530

    Intial commit with Html,css and js verison

---
+++ src/main/java/com/example/RealLanguage/ChatMessage.java
@@ -0,0 +1,19 @@
+package com.example.RealLanguage;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+	private String content;
+	private String sender;
+	private MessageType messageType;
+
+}
---
+++ src/main/java/com/example/RealLanguage/MessageType.java
@@ -0,0 +1,8 @@
+package com.example.RealLanguage;
+
+public enum MessageType {
+	CHAT,
+	JOIN,
+	LEAVE
+
+}
